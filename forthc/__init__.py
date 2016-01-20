1bd8b666b16744c74fc5088f019a2aed8150d244
C-Elegans <mtnolan2640@gmail.com>
2016-01-19 19:01:43 -0500

    Add command line opts, quiet option

--- forthc/__init__.py
+++ forthc/__init__.py
@@ -1,15 +1,39 @@
 #!/usr/bin/python
 import words,stack_as
-import sys, re
+import sys, re, getopt
 def main():
         
     whitespace = re.compile(r"[ \t\n]+")
     number = re.compile(r"-?[0-9]+")
-    if(len(sys.argv) != 3):
-        print "Usage: ./compiler.py [forth] [assembly]"
+    inputfile = ''
+    outputfile = ''
+    quiet = False
+   
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"hqi:o:",["ifile=","ofile=","quiet"])
+       
+        for opt, arg in opts:
+            if opt == '-h':
+                print 'Usage: compiler.py -i <inputfile> -o <outputfile>'
+                sys.exit()
+            elif opt in ("-i", "--ifile"):
+                inputfile = arg
+            elif opt in ("-o", "--ofile"):
+                outputfile = arg
+            elif opt in ("-q", "--quiet"):
+                quiet = True
+    except getopt.GetoptError:
+        print 'Usage: compiler.py -i <inputfile> -o <outputfile>'
+        sys.exit(2)
+    
+    
+    
+    if inputfile != '' and outputfile != '':
+        lines = open(inputfile,"r").read()
+        outfile = open(outputfile,"w")
+    else:
+        print "No input or output specified"
         sys.exit(-1)
-    lines = open(sys.argv[1],"r").read()
-    outfile = open(sys.argv[2],"w")
     tokens = whitespace.split(lines)
     tokens_iter = iter(tokens)
     ifthen_count = 0
@@ -25,11 +49,9 @@ def main():
             elif token in words.words:
                 asm_text += "call "+token+ "\n"
             elif token in words.other_words:
-                print "other words " + token
                 asm_text += "call "+token+ "\n"
                 words.copy_word(token)
             elif token == "IF":
-                print token
                 asm_text += "cjump then"+str(ifthen_count)+"\n"
             elif token == "THEN":
                 asm_text += "then"+str(ifthen_count) + ":\n"
@@ -37,7 +59,6 @@ def main():
             elif token == ":":
                 new_word = []
                 for w_token in tokens_iter:
-                    print w_token
                     if w_token is ";":
                         break
                     else:
@@ -55,7 +76,10 @@ def main():
     asm_text +="jump end\n"
     asm_text +=words.wordlines
     asm_text +="end: nop\n"
-    print asm_text
+    if not quiet:
+        print asm_text
     out_lines = asm_text.split("\n")
     arr = stack_as.main(out_lines)
     outfile.write(bytearray(arr))
+if __name__ == "__main__":
+    main()
